f0cb4afada72ff4299b486864cd57e7476fde8d2
Sudhar San <34421911+SudharsanK58@users.noreply.github.com>
2024-04-03 17:01:57 +0530

    Added TOF distance to MQTT time concept

--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,15 @@
 #include <PubSubClient.h>
 #include <WiFi.h>
 
-const char* ssid = "No Internet Connection";
-const char* password = "dontaskme";
+const char* ssid = "ACTFIBERNET";
+const char* password = "act12345";
 const char* mqttServer = "mqtt.zig-web.com";
 const int mqttPort = 1883;
 bool distance_below_1_foot = false;
 bool printedMessage = false;
-
+bool messagePrinted = false;
+unsigned long start_time = 0; // Time when user comes within 1 foot range
+unsigned long end_time = 0;   // Time when valid MQTT data is received
 
 WiFiClient espClient;
 PubSubClient client(espClient);
@@ -22,8 +24,10 @@ unsigned long TOF_distance = 0;
 unsigned char TOF_status = 0;
 unsigned int TOF_signal = 0;
 unsigned char TOF_check = 0;
+
 void setup_wifi();
 void setup_mqtt();
+void printIncomingMQTT(byte* payload, unsigned int length);
 
 void setup() {
   Serial.begin(115200);
@@ -57,25 +61,22 @@ void callback(char* topic, byte* payload, unsigned int length) {
   for (int i = 0; i < length; i++) {
     message += (char)payload[i];
   }
-
-
-  // Check if the distance is under 1 foot
-  float distance_feet = TOF_distance * 0.00328084;
-    Serial.println("Data " + String(distance_feet));
-  if (distance_feet < 1 && distance_feet != 0.00 && !printedMessage) {  
-    // Print the received message from MQTT
-    if (!(message.startsWith("301") || message.startsWith("SOS") || message.startsWith("STOP"))) {
-        Serial.print("Message received on topic: ");
-        Serial.println(topic);
-        Serial.print("Message: ");
-        Serial.println(message);
-        printedMessage = true;
-      }
-  }else{
-     printedMessage = false;
+  
+  if (distance_below_1_foot && !printedMessage && !message.startsWith("301") && !message.startsWith("SOS") && !message.startsWith("STOP")) {
+    Serial.print("Message received on topic: ");
+    Serial.println(topic);
+    Serial.print("Message: ");
+    Serial.println(message);
+    printedMessage = true;
+    end_time = millis(); // Record time when valid MQTT data is received
+    unsigned long time_taken = end_time - start_time;
+    Serial.print("Time taken: ");
+    Serial.print(time_taken);
+    Serial.println(" milliseconds");
+    String data_with_time = message + "#BEN" + String(time_taken); 
+    client.publish("04:e9:e5:16:f6:3d/ben", data_with_time.c_str());
   }
 }
-
 
 void setup_mqtt() {
   client.setServer(mqttServer, mqttPort);
@@ -87,7 +88,7 @@ void reconnect() {
     Serial.print("Attempting MQTT connection...");
     if (client.connect("ESP32Client")) {
       Serial.println("connected");
-      client.subscribe("04:e9:e5:16:f6:3d/nfc");
+      client.subscribe("04:e9:e5:16:f6:3d/device");
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
@@ -96,7 +97,6 @@ void reconnect() {
     }
   }
 }
-
 
 bool verifyCheckSum(unsigned char data[], unsigned char len) {
   TOF_check = 0;
@@ -129,23 +129,43 @@ void loop() {
       if ((TOF_data[j] == TOF_header[0] && TOF_data[j + 1] == TOF_header[1] && TOF_data[j + 2] == TOF_header[2]) && (verifyCheckSum(&TOF_data[j], TOF_length))) {
         if (((TOF_data[j + 12]) | (TOF_data[j + 13] << 8)) == 0) {
           Serial.println("Out of range!");
+          distance_below_1_foot = false;
+          printedMessage = false;
+          messagePrinted = false;
         } else {
           TOF_distance = (TOF_data[j + 8]) | (TOF_data[j + 9] << 8) | (TOF_data[j + 10] << 16);
-          // Serial.print("TOF distance is: ");
-          // Serial.print(distance_feet, 2); // Print with 2 decimal places
-          // Serial.println(" feet");
-          // Serial.println(""); 
-          // if (distance_feet < 1) {
-          //   distance_below_1_foot = true;
-          // }
+          float distance_feet = TOF_distance * 0.00328084;
+          Serial.print("TOF distance is: ");
+          Serial.print(distance_feet, 2); // Print with 2 decimal places
+          Serial.println(" feet");
+          Serial.println(""); 
+          
+          // Check if distance is below 1 foot
+          if (distance_feet < 2) {
+            distance_below_1_foot = true;
+            if (!start_time) {
+              start_time = millis(); // Record time when user comes within 1 foot range
+            }
+          } else {
+            distance_below_1_foot = false;
+            printedMessage = false;
+            messagePrinted = false;
+            start_time = 0; // Reset start_time when user goes out of 1 foot range
+          }
         }
         break;
       }
     }
   }
-
+  
   if (!client.connected()) {
     reconnect();
   }
   client.loop();
+
+  // Reset distance_below_1_foot flag after printing MQTT message
+  if (printedMessage && !messagePrinted) {
+    distance_below_1_foot = false;
+    messagePrinted = true;
+  }
 }
